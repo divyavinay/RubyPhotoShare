0f3694ce12912b92db45697152f74fec915e7b04
Divya <divya.basappa@gmail.com>
2017-04-15 22:40:04 -0700

    Added Back link in display page

--- ProjectR1/app/controllers/photos_controller.rb
+++ ProjectR1/app/controllers/photos_controller.rb
@@ -23,7 +23,6 @@ class PhotosController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_photo
-      @photo = Photo.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
