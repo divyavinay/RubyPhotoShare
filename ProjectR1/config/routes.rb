79a76ee3d137e251d6160baf466aa2abddb25aac
Divya <divya.basappa@gmail.com>
2017-04-19 11:43:59 -0700

    Added route users/index to map to users_path

--- ProjectR1/config/routes.rb
+++ ProjectR1/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'users/index',to:'users#index'
   resources :photos do
   member do
     get 'display',controller:'photos',action:'display',to_param:[:photo_id]
